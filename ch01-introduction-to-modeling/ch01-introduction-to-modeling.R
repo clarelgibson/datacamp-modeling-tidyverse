d7ae3cc1ab65fe8a5ea12c39149feee3b9fd6428
Clare Gibson <clarelgibson@gmail.com>
2021-09-15 22:05:35 +0100

    Adds Chapter 1 directory

---
+++ ch01-introduction-to-modeling/ch01-introduction-to-modeling.R
@@ -0,0 +1,10 @@
+# Title:   Introduction to Modeling
+# File:    ch01-introduction-to-modeling.R
+# Course:  Datacamp Modeling with Data in the Tidyverse
+
+# SUMMARY #####################################################################
+# This chapter will introduce you to some background theory and terminology for
+# modeling, in particular, the general modeling framework, the difference
+# between modeling for explanation and modeling for prediction, and the
+# modeling problem. Furthermore, you'll start performing your first exploratory
+# data analysis, a crucial first step before any formal modeling.
